dfdd8882615fd227775a1329e33f10937385ea31
William Oldham <wegg7250@gmail.com>
2021-11-23 12:33:34 +0000

    Add error handling to express

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,20 @@ const express = require("express")
 const config = require("./config")
 const morgan = require("morgan")
 
+const { respondError } = require("./utils/http")
 const { setupRoutes } = require("./routes/routes")
 
 const app = express()
 
 app.use(express.json())
 app.use(morgan("dev"))
+
+app.use((err, req, res, next) => {
+
+    console.error(`Error 500: ${err.message}`)
+    respondError(res, err.message, { stack: err.stack })
+
+});
 
 setupRoutes(app)
 
