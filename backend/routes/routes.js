3774cf56f341286dd468c17836616755b69aa95b
William Oldham <wegg7250@gmail.com>
2021-11-30 08:13:27 +0000

    Backend: Add dummy route to test frontend

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,5 +3,8 @@ const auth = require("./auth")
 module.exports = {
     setupRoutes (app) {
         app.use("/auth", auth)
+        app.get("/user", (req, res) => {
+            res.send("Hi!")
+        })
     }
 }
