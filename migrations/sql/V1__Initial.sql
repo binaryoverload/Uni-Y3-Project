618c41a097341ee8d1675cebcdf9a8663cf218b4
William Oldham <wegg7250@gmail.com>
2021-12-04 11:48:57 +0000

    Migrations: Create initial SQL migration

---
+++ migrations/sql/V1__Initial.sql
@@ -0,0 +1,30 @@
+CREATE TABLE users (
+    id serial,
+    username varchar(50) not null,
+    password varchar(255) not null,
+    updated_at timestamp default timezone('UTC', now()),
+    security_stamp integer default 0,
+    checksum  varchar(70) GENERATED ALWAYS AS (sha256((password::text || security_stamp::text)::bytea)) STORED,
+    first_name varchar(255),
+    last_name varchar(255),
+
+    primary key (id)
+);
+
+CREATE TABLE system_properties (
+    key varchar(50),
+    value text not null,
+    
+    primary key (key)
+);
+
+CREATE TABLE enrolment_tokens (
+    id serial,
+    token varchar(16) not null,
+    created_at timestamp default timezone('UTC', now()),
+    expires_at timestamp,
+    usage_current integer default 0,
+    usage_limit integer,
+
+    primary key (id)
+);
