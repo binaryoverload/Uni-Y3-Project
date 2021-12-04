327312c6a3dab3db86c4c98cfc92fbb3f0f0f3e9
William Oldham <wegg7250@gmail.com>
2021-12-04 21:37:32 +0000

    Backend: Add get user endpoint

--- backend/models/user.js
+++ backend/models/user.js
@@ -23,4 +23,18 @@ async function createUser(data) {
     }
 }
 
-module.exports = { createUser }
+async function getUser(username) {
+    const query = `
+        SELECT id,username,password,first_name,last_name,checksum FROM users WHERE username=$1
+    `
+    const result = await queryPool(query, [username])
+
+    // It is okay to check against 1, since there will only ever be 1 instance of a username due to unique constraints
+    if (result.rowCount !== 1) {
+        return null
+    }
+
+    return result.rows[0]
+}
+
+module.exports = { createUser, getUser }
