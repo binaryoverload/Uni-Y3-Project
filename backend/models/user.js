16eafd4f19f3aac4f31d6c5130f70e53e7405d78
William Oldham <wegg7250@gmail.com>
2021-12-05 19:45:44 +0000

    Backend: User model addition

--- backend/models/user.js
+++ backend/models/user.js
@@ -30,4 +30,36 @@ async function getUserByUsername (username) {
     return result.rows[0]
 }
 
-module.exports = { createUser, getUserByUsername }
+async function getUserById (id) {
+    const result = await queryPool(queries.user.getId, [id])
+
+    // It is okay to check against 1, since there will only ever be 1 instance of a username due to unique constraints
+    if (result.rowCount !== 1) {
+        return null
+    }
+
+    return result.rows[0]
+}
+
+async function getAllUsers () {
+    const result = await queryPool(queries.user.getAll)
+
+    return result.rows
+}
+
+async function deleteUser (id) {
+    const result = await queryPool(queries.enrolmentTokens.delete, [id])
+
+    if (result.rowCount !== 1) {
+        // TODO something
+        return
+    }
+
+    return true
+}
+
+async function updateUser (data) {
+    const { username, first_name, last_name, password } = data
+}
+
+module.exports = { createUser, getUserByUsername, getUserById, getAllUsers, deleteUser }
