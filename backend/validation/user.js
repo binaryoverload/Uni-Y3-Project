4813fba13fd33becdccb3dc95a95668521e6a5a7
William Oldham <wegg7250@gmail.com>
2021-12-06 14:24:05 +0000

    Backend: User route validation

--- backend/validation/user.js
+++ backend/validation/user.js
@@ -1,4 +1,9 @@
-const { body } = require("express-validator")
+const { body, param } = require("express-validator")
+
+const userIdValidator = param("id", "User ID is required")
+    .exists()
+    .isUUID(4)
+    .withMessage("User ID must be a UUID v4")
 
 module.exports = {
     userPost: [
@@ -22,6 +27,39 @@ module.exports = {
             .withMessage("The first name must be at least 1 character long"),
         body("last_name", "The last name is required")
             .exists()
+            .isString()
+            .trim()
+            .isLength({ min: 1 })
+            .withMessage("The last name must be at least 1 character long")
+    ],
+    userGetId: [
+        userIdValidator
+    ],
+    userDelete: [
+        userIdValidator
+    ],
+    userPut: [
+        userIdValidator,
+        body("username", "The username must be a string")
+            .optional()
+            .isString()
+            .trim()
+            .isLength({ min: 1 })
+            .withMessage("The username must be at least 1 character long"),
+        body("password", "The password must be a string")
+            .optional()
+            .isString()
+            .trim()
+            .isLength({ min: 1 })
+            .withMessage("The password must be at least 1 character long"),
+        body("first_name", "The first name must be a string")
+            .optional()
+            .isString()
+            .trim()
+            .isLength({ min: 1 })
+            .withMessage("The first name must be at least 1 character long"),
+        body("last_name", "The last name must be a string")
+            .optional()
             .isString()
             .trim()
             .isLength({ min: 1 })
