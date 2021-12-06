f8e17698b3d066fe01787d2568cd76d20af044bd
William Oldham <wegg7250@gmail.com>
2021-12-06 14:53:49 +0000

    Backend: Enrolment Token Validator

---
+++ backend/validation/enrolmentTokens.js
@@ -0,0 +1,23 @@
+const { body, param } = require("express-validator")
+
+const tokenIdValidator = param("id", "ID must be a UUIDv4")
+    .exists()
+    .isUUID(4)
+
+module.exports = {
+    tokenGet: [
+        tokenIdValidator
+    ],
+    tokenDelete: [
+        tokenIdValidator
+    ],
+    tokenUpdate: [
+        tokenIdValidator,
+        body("expires_at", "Expires at must be null or a epoch value")
+            .optional({ nullable: true })
+            .isInt(),
+        body("usage_limit", "Usage limit must be null or a epoch value")
+            .optional({ nullable: true })
+            .isInt(),
+    ]
+}
