134c6cc1d99f02698bdc6c349ae8517e38336bf5
William Oldham <wegg7250@gmail.com>
2021-12-18 23:07:18 +0000

    Backend: Policy create, get and getAll models

--- backend/models/policies.js
+++ backend/models/policies.js
@@ -1,5 +1,36 @@
 const { knex } = require("../setup/db")
 const { handlePostgresError } = require("../utils/errorHandling")
+const { getUserById } = require("./user")
+const { BadRequest } = require("../utils/exceptions")
 
 const policiesTableName = "policies"
 
+async function createPolicy (created_by) {
+
+    if (!await getUserById(created_by)) {
+        throw new BadRequest(`User with ID ${created_by} not found`)
+    }
+
+    return await knex(policiesTableName)
+        .insert({
+            created_by
+        })
+        .returning("id")
+        .first()
+        .catch(handlePostgresError)
+}
+
+async function getById (id) {
+    return await knex(policiesTableName)
+        .withRelations(knex(policyItemsTableName), "id", "policy_id")
+        .where("id", id)
+        .first()
+        .catch(handlePostgresError)
+}
+
+async function getAll () {
+    return await knex(policiesTableName)
+        .withRelations(knex(policyItemsTableName), "id", "policy_id")
+        .first()
+        .catch(handlePostgresError)
+}
