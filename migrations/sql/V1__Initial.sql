da5744ffa6e71b2a356101df545325087653b73f
William Oldham <wegg7250@gmail.com>
2021-12-19 11:13:39 +0000

    Edit SQL to enforce not-null on default values

--- migrations/sql/V1__Initial.sql
+++ migrations/sql/V1__Initial.sql
@@ -2,8 +2,8 @@ CREATE TABLE users (
     id uuid default gen_random_uuid(),
     username varchar(50) not null,
     password varchar(255) not null,
-    updated_at timestamp default timezone('UTC', now()),
-    security_stamp integer default 0,
+    updated_at timestamp not null default timezone('UTC', now()),
+    security_stamp integer not null default 0,
     checksum  varchar(70) GENERATED ALWAYS AS (encode(sha256((password::text || security_stamp::text)::bytea), 'hex')) STORED,
     first_name varchar(255),
     last_name varchar(255),
@@ -22,9 +22,9 @@ CREATE TABLE system_properties (
 CREATE TABLE enrolment_tokens (
     id uuid default gen_random_uuid(),
     token varchar(16) not null,
-    created_at timestamp default timezone('UTC', now()),
+    created_at timestamp not null default timezone('UTC', now()),
     expires_at timestamp,
-    usage_current integer default 0,
+    usage_current integer not null default 0,
     usage_limit integer,
 
     primary key (id),
@@ -33,7 +33,7 @@ CREATE TABLE enrolment_tokens (
 
 CREATE TABLE policies (
     id uuid default gen_random_uuid(),
-    created_at timestamp default timezone('UTC', now()),
+    created_at timestamp not null default timezone('UTC', now()),
     created_by uuid not null,
 
     primary key (id)
@@ -46,8 +46,8 @@ CREATE TABLE policy_items (
     policy_id uuid not null,
     policy_order int not null,
     type policy_item_type not null,
-    stop_on_error boolean not null,
-    data jsonb not null,
+    stop_on_error boolean not null default 'true',
+    data jsonb not null default '{}',
 
     primary key (id),
     foreign key (policy_id) references policies(id) on delete cascade,
