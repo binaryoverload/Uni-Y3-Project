2d3689fa0725e2fb45094870dd6c17ae35463b73
William Oldham <wegg7250@gmail.com>
2022-01-05 23:06:09 +0000

    Client: HelloAck and fix decoding offsets

--- client/packets/hello_ack.go
+++ client/packets/hello_ack.go
@@ -2,14 +2,14 @@ package packets
 
 import "errors"
 
-type HelloPacket struct {
+type HelloAckPacket struct {
 	PublicKey []byte
 	AesData   []byte
 }
 
-func EncodeHello(packet HelloPacket) []byte {
+func EncodeHelloAck(packet HelloAckPacket) []byte {
 	opCode := make([]byte, 1)
-	opCode[0] = Hello
+	opCode[0] = HelloAck
 
 	output := append(opCode, packet.PublicKey...)
 	output = append(output, packet.AesData...)
@@ -17,11 +17,11 @@ func EncodeHello(packet HelloPacket) []byte {
 	return output
 }
 
-func decodeHello(data []byte) (DataPacket, error) {
+func DecodeHelloAck(data []byte) (DataPacket, error) {
 	if len(data) <= (1 + 33 + 32) {
 		return nil, errors.New("hello data must be at least 66 bytes")
 	}
-	return HelloPacket{
+	return HelloAckPacket{
 		data[1:34],
 		data[34:],
 	}, nil
