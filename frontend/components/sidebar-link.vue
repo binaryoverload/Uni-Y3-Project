f72a7f6e78e1582f20d1a82f941af2e927abdc27
William Oldham <wegg7250@gmail.com>
2022-01-10 16:13:26 +0000

    Frontend: Implement design

---
+++ frontend/components/sidebar-link.vue
@@ -0,0 +1,22 @@
+<template>
+    <div :class="styles">
+        <span class="w-6 mr-4">
+            <font-awesome-icon :icon="icon" :class="active ? 'text-indigo-600 ' : ''"/>
+        </span><slot/>
+        </div>
+</template>
+
+<script>
+export default {
+    props: {
+        active: Boolean,
+        icon: String
+    },
+    computed: {
+        styles() {
+            let common = "w-full px-16 py-4 text-xl cursor-pointer"
+            return this.active ? common + " text-white bg-indigo-400 hover:bg-opacity-20 bg-opacity-10" : common + " text-slate-400 hover:text-slate-200"
+        }
+    }
+}
+</script>
---
+++ frontend/layouts/dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex h-full">
+        <div class="flex flex-col text-white w-96 bg-slate-900">
+            <p class="px-16 py-10 text-4xl font-medium">Themis</p>
+            <sidebar-link active icon="scroll">Policies</sidebar-link>
+            <sidebar-link icon="desktop">Clients</sidebar-link>
+            <sidebar-link icon="ticket-alt">Enrolment Tokens</sidebar-link>
+            <sidebar-link icon="user">Users</sidebar-link>
+            <sidebar-link icon="sign-out-alt" class="mt-auto mb-4">Logout</sidebar-link>
+        </div>
+        <div class="flex-1 px-24 py-20">
+            <nuxt/>
+        </div>
+    </div>
+</template>
