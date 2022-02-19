359bee903521df4e11436fd76409ef75c9ff39ea
William Oldham <wegg7250@gmail.com>
2022-02-19 21:46:17 +0000

    Backend: Create files models

---
+++ backend/models/files.js
@@ -0,0 +1,64 @@
+const { knex } = require("../setup/db")
+const { handlePostgresError } = require("../utils/errorHandling")
+
+const FILES_TABLE_NAME = "files"
+
+async function createFile (data) {
+    const { id, name, original_filename, hash, size } = data
+
+    return await knex(FILES_TABLE_NAME)
+        .insert({
+            id,
+            name,
+            original_filename,
+            hash,
+            size
+        })
+        .returning("id")
+        .then(r => {
+            return { id: r[0] }
+        })
+        .catch(handlePostgresError)
+}
+
+async function deleteFile (id) {
+    return await knex(FILES_TABLE_NAME)
+        .where("id", id)
+        .delete()
+        .returning("id")
+        .catch(handlePostgresError)
+}
+
+async function getFileById (id) {
+    return await knex(FILES_TABLE_NAME)
+        .select([
+            "id",
+            "name",
+            "original_filename",
+            "hash",
+            "size",
+            "updated_at"])
+        .where("id", id)
+        .first()
+        .catch(handlePostgresError)
+}
+
+async function getAllFiles (id) {
+    return await knex(FILES_TABLE_NAME)
+        .select([
+            "id",
+            "name",
+            "original_filename",
+            "hash",
+            "size",
+            "updated_at"])
+        .first()
+        .catch(handlePostgresError)
+}
+
+module.exports = {
+    createFile,
+    deleteFile,
+    getFileById,
+    getAllFiles
+}
