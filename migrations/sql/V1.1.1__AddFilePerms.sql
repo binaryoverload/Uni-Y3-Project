70ee956b88cc56f86159e9b9779a30a46363ff51
William Oldham <wegg7250@gmail.com>
2022-03-06 23:03:43 +0000

    Migration: Add default to not-null column

--- migrations/sql/V1.1.1__AddFilePerms.sql
+++ migrations/sql/V1.1.1__AddFilePerms.sql
@@ -2,7 +2,7 @@ alter table files
     add permission smallint default 644;
 
 alter table files
-    add owner_user text not null;
+    add owner_user text not null default 'root';
 
 alter table files
-    add owner_group text;
+    add owner_group text default 'root';
