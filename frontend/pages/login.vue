15021fdddc03871a4dfb1a01d0169f7f9597ede1
William Oldham <wegg7250@gmail.com>
2022-03-13 12:03:27 +0000

    Frontend: increase placeholder contract and focus username field on mount

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -15,17 +15,18 @@
           <input
             type="text"
             id="username"
+            ref="username"
             name="username"
             v-model="username"
             placeholder="Username"
-            class="w-full border rounded-t border-slate-300 focus:border-slate-400 focus:ring-2 focus:ring-opacity-50 focus:ring-slate-400 placeholder-slate-300 text-slate-900" />
+            class="w-full border rounded-t border-slate-300 focus:border-slate-400 focus:ring-2 focus:ring-opacity-50 focus:ring-slate-400 placeholder-slate-400 text-slate-900" />
           <input
             type="password"
             id="password"
             name="password"
             v-model="password"
             placeholder="Password"
-            class="w-full border rounded-b border-slate-300 focus:border-slate-400 focus:ring-2 focus:ring-opacity-50 focus:ring-slate-400 placeholder-slate-300 text-slate-900" />
+            class="w-full border rounded-b border-slate-300 focus:border-slate-400 focus:ring-2 focus:ring-opacity-50 focus:ring-slate-400 placeholder-slate-400 text-slate-900" />
         </div>
         <t-button class="mx-auto mt-10">Login</t-button>
       </form>
@@ -42,7 +43,13 @@ export default {
     }
   },
   middleware: ["guest"],
+  mounted() {
+    this.focusInput()
+  },
   methods: {
+    focusInput() {
+      this.$refs.username.focus()
+    },
     async submitLogin() {
       const { username, password } = this
       try {
