929a5a6ba46c4cd15eebd490a554301870b2b1b1
William Oldham <wegg7250@gmail.com>
2022-03-13 11:57:39 +0000

    Frontend: Change login page to have combined field

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,9 +1,8 @@
 <template>
   <div id="app" class="flex items-center justify-center h-full bg-slate-800">
-    <div class="flex flex-col items-center py-10 space-y-4 text-white rounded px-14 bg-slate-900">
-      <p class="self-start text-4xl font-medium">
-        Themis
-      </p>
+    <div
+      class="flex flex-col items-center py-10 space-y-10 text-white rounded px-14 bg-slate-900">
+      <p class="self-start text-4xl font-medium">Themis Login</p>
       <t-alert
         :show="!!$store.state.alert.message"
         :variant="$store.state.alert.type"
@@ -12,16 +11,22 @@
         >{{ $store.state.alert.message }}
       </t-alert>
       <form @submit.prevent="submitLogin" class="">
-        <div>
-          <label for="username" class="block text-sm">Username</label>
-          <input type="text" id="username" name="username" v-model="username" placeholder="Username" class="w-full border rounded border-slate-300 focus:border-slate-400 focus:ring-2 focus:ring-opacity-50 focus:ring-slate-400 placeholder-slate-300 text-slate-900" />
+        <div class="flex flex-col">
+          <input
+            type="text"
+            id="username"
+            name="username"
+            v-model="username"
+            placeholder="Username"
+            class="w-full border rounded-t border-slate-300 focus:border-slate-400 focus:ring-2 focus:ring-opacity-50 focus:ring-slate-400 placeholder-slate-300 text-slate-900" />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            v-model="password"
+            placeholder="Password"
+            class="w-full border rounded-b border-slate-300 focus:border-slate-400 focus:ring-2 focus:ring-opacity-50 focus:ring-slate-400 placeholder-slate-300 text-slate-900" />
         </div>
-
-        <div>
-          <label for="password" class="block mt-4 text-sm">Password</label>
-          <input type="password" id="password" name="password" v-model="password" placeholder="Password" class="w-full border rounded border-slate-300 focus:border-slate-400 focus:ring-2 focus:ring-opacity-50 focus:ring-slate-400 placeholder-slate-300 text-slate-900" />
-        </div>
-
         <t-button class="mx-auto mt-10">Login</t-button>
       </form>
     </div>
@@ -34,32 +39,32 @@ export default {
     return {
       username: "",
       password: "",
-    };
+    }
   },
   middleware: ["guest"],
   methods: {
     async submitLogin() {
-      const { username, password } = this;
+      const { username, password } = this
       try {
         await this.$auth.loginWith("local", {
           data: {
             username,
             password,
           },
-        });
-        this.$store.commit("alert/clearAlert");
+        })
+        this.$store.commit("alert/clearAlert")
       } catch (reason) {
         this.$store.commit("alert/setAlert", {
           message: reason.request.statusText || reason.message,
           type: "danger",
-        });
+        })
       }
     },
   },
   head() {
     return {
       title: "Themis - Login",
-    };
+    }
   },
-};
+}
 </script>
