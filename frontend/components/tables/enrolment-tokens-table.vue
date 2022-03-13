c29f0cf91c841a19423ab44920f4b7a066f903e3
William Oldham <wegg7250@gmail.com>
2022-03-13 11:56:47 +0000

    Frontend: Format enrolment tokens table

--- frontend/components/tables/enrolment-tokens-table.vue
+++ frontend/components/tables/enrolment-tokens-table.vue
@@ -3,41 +3,41 @@
 </template>
 
 <script>
-import table from "./table.vue";
+import table from "./table.vue"
 
 const schema = [
   {
     display: "link",
-    url: (row) => "/enrolment-tokens/" + row.id,
+    url: row => "/enrolment-tokens/" + row.id,
     key: "name",
     heading: "Name",
   },
   {
     display: "statusIcon",
-    key: (row) => {
-      if (!row.expires_at && !row.usage_limit) return "online";
+    key: row => {
+      if (!row.expires_at && !row.usage_limit) return "online"
 
-      const usageRatio = row.usage_current / row.usage_limit;
+      const usageRatio = row.usage_current / row.usage_limit
 
       if (usageRatio >= 0.8) {
-        return "offline";
+        return "offline"
       } else if (usageRatio < 0.8 && usageRatio >= 0.6) {
-        return "warning";
+        return "warning"
       }
 
-      if (row.usage_current >= row.usage_limit) return "offline";
+      if (row.usage_current >= row.usage_limit) return "offline"
 
-      const timeUntilExpiry = Date.now() - row.expires_at.getTime();
+      const timeUntilExpiry = Date.now() - row.expires_at.getTime()
       if (isNaN(timeUntilExpiry)) {
-        return "unknown";
+        return "unknown"
       } else if (timeUntilExpiry < 60 * 10 * 1000) {
         // < 10 Minutes is considered healthy
-        return "online";
+        return "online"
       } else if (timeUntilExpiry < 60 * 60 * 1000) {
         // < 1 Hour
-        return "warning";
+        return "warning"
       } else {
-        return "offline";
+        return "offline"
       }
     },
     heading: "Status",
@@ -67,26 +67,26 @@ const schema = [
     actions: [
       {
         icon: "copy",
-        onClick: (row) => {
+        onClick: row => {
           navigator.clipboard.writeText(row.token).then(
             function () {
-              alert("Copied enrolment token to clipboard!");
+              alert("Copied enrolment token to clipboard!")
             },
             function (err) {
-              console.error("Could not copy enrolment token: ", err);
+              console.error("Could not copy enrolment token: ", err)
             }
-          );
+          )
         },
       },
       {
         icon: "download",
-        onClick: (row) => {
-          alert("downloaded!!");
+        onClick: row => {
+          alert("downloaded!!")
         },
       },
     ],
   },
-];
+]
 
 export default {
   components: { CustomTable: table },
@@ -102,7 +102,7 @@ export default {
   data() {
     return {
       schema,
-    };
+    }
   },
-};
+}
 </script>
