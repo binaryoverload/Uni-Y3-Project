5297d4a4fee244158ca687a92f153f1e47c3977b
William Oldham <wegg7250@gmail.com>
2022-03-14 17:32:41 +0000

    Frontend: Add section head comp and inputbox comp

---
+++ frontend/components/section-header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mt-8 mb-8">
+    <p class="font-bold">{{ title }}</p>
+    <p class="text-slate-600">{{ subtitle }}</p>
+    <hr class="mt-2" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    subtitle: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
--- frontend/pages/policies/create.vue
+++ frontend/pages/policies/create.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
-      <p class="mb-10 text-5xl font-bold leading-[3rem]">Create Policies</p>
-    <div>
-    </div>
+    <p class="mb-10 text-5xl font-bold leading-[3rem]">Create Policies</p>
+    <section-header
+      title="Account information"
+      subtitle="For logging in and such." />
+    <input-box placeholder="Test" />
+
+    <section-header
+      title="Personal information"
+      subtitle="Information about the user." />
+    <div></div>
   </div>
 </template>
 
 <script>
 export default {
   layout: "dashboard",
-  middleware: "authed"
-};
+  middleware: "authed",
+}
 </script>
