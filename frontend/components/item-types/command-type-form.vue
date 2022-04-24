82131414dbb3e564ed9d009ee34b83193cde8621
William Oldham <wegg7250@gmail.com>
2022-04-24 12:42:10 +0100

    Frontend: Command and package item type forms

--- frontend/components/item-types/command-type-form.vue
+++ frontend/components/item-types/command-type-form.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="space-y-2">
     <form-input
       label="Command"
       :value="value.command"
@@ -13,7 +13,35 @@
       label="Working Directory"
       :value="value.working_directory"
       @input="$emit('input', { ...value, working_directory: $event })" />
-    Environment
+    <div class="space-y-2">
+      <p class="text-sm font-bold">Environment Variables</p>
+      <div>
+        <p
+          v-for="envKey in Object.keys(value.env || {})"
+          :key="envKey"
+          class="flex items-center">
+          <span class="font-mono">{{ envKey }}</span> =
+          <span class="font-mono">{{ value.env[envKey] }}</span>
+          <font-awesome-icon
+            icon="times"
+            class="ml-2 text-red-600 cursor-pointer"
+            @click="removeEnv(envKey)" />
+        </p>
+      </div>
+      <form @submit.prevent="addEnv" class="flex gap-1">
+        <input-box
+          placeholder="Variable Name"
+          ref="envName"
+          required
+          v-model="envName" />
+        <input-box
+          placeholder="Variable Value"
+          ref="envValue"
+          required
+          v-model="envValue" />
+        <t-button class="ml-1">Add</t-button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -21,6 +49,28 @@
 export default {
   props: {
     value: Object,
+  },
+  data() {
+    return {
+      envName: "",
+      envValue: "",
+    }
+  },
+  methods: {
+    addEnv() {
+      if (!this.envName | !this.envValue) {
+        return
+      }
+      const env = { ...this.value.env, [this.envName]: this.envValue }
+      this.envName = ""
+      this.envValue = ""
+      this.$emit("input", { ...this.value, env })
+    },
+    removeEnv(envName) {
+      const env = { ...this.value.env }
+      delete env[envName]
+      this.$emit("input", { ...this.value, env })
+    },
   },
 }
 </script>
--- frontend/components/item-types/package-type-form.vue
+++ frontend/components/item-types/package-type-form.vue
@@ -1,11 +1,66 @@
 <template>
-  <div></div>
+  <div class="space-y-2">
+    <form-input
+      label="Package Action"
+      :options="[
+        {
+          key: 'install',
+          title: 'Install',
+        },
+        {
+          key: 'uninstall',
+          title: 'Uninstall',
+        },
+      ]"
+      component="fancyRadio"
+      :value="value.action || 'install'"
+      @input="$emit('input', { ...value, action: $event })"
+      required />
+    <div class="space-y-2">
+      <p class="text-sm font-bold">Packages</p>
+      <div>
+        <p v-for="pack in value.packages" :key="pack" class="flex items-center">
+          <span class="font-mono">{{ pack }}</span>
+          <font-awesome-icon
+            icon="times"
+            class="ml-2 text-red-600 cursor-pointer"
+            @click="removePackage(pack)" />
+        </p>
+      </div>
+      <form @submit.prevent="addPackage" class="flex gap-1">
+        <input-box placeholder="Package" required v-model="packInput" />
+        <t-button class="ml-1">Add</t-button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   props: {
     value: Object,
+  },
+  data() {
+    return {
+      packInput: "",
+    }
+  },
+  methods: {
+    addPackage() {
+      const packages = this.value.packages || []
+      if (packages.indexOf(this.packInput) === -1) {
+        packages.push(this.packInput)
+      }
+      this.$emit("input", { ...this.value, packages })
+    },
+    removePackage(pack) {
+      const packages = this.value.packages || []
+      const index = packages.indexOf(pack)
+      if (index > -1) {
+        packages.splice(index, 1)
+      }
+      this.$emit("input", { ...this.value, packages })
+    },
   },
 }
 </script>
