19a019c3df8a46a851734d5c5f27cfc3d63828a6
William Oldham <wegg7250@gmail.com>
2022-05-02 13:38:32 +0100

    Frontend: Add delete buttons to policy items

--- frontend/components/policy-item-cards/command-item-card.vue
+++ frontend/components/policy-item-cards/command-item-card.vue
@@ -1,7 +1,6 @@
 <template>
-  <nuxt-link
-    class="flex items-center min-w-[200px] px-4 py-2 space-x-4 border rounded border-slate-300"
-    :to="`/policy-items/${item.id}`">
+  <div
+    class="flex items-center min-w-[200px] px-4 py-2 space-x-4 border rounded border-slate-300">
     <div
       class="flex items-center self-stretch pr-4 -my-2 text-xl border-r border-slate-300 text-slate-600">
       <span>#{{ item.policy_order }}</span>
@@ -18,15 +17,30 @@
         <span>{{ item.data.args.length }} args</span>
       </p>
     </div>
-  </nuxt-link>
+    <div>
+      <div
+        class="px-2 py-1.5 text-red-500 bg-red-200 rounded cursor-pointer hover:bg-red-300 hover:text-red-600 focus:ring-red-500 focus:border-red-500 focus:ring-2 focus:outline-none focus:ring-opacity-50"
+        tabindex="0"
+        @click="deleteItem">
+        <font-awesome-icon icon="times" class="block" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { deletePolicyItem } from "~/utils/actions"
+
 export default {
   props: {
     item: {
       type: Object,
       required: true,
+    },
+  },
+  methods: {
+    async deleteItem() {
+      deletePolicyItem.call(this)
     },
   },
   computed: {
--- frontend/utils/actions.js
+++ frontend/utils/actions.js
@@ -20,6 +20,35 @@ async function deleteEntity(url, callback) {
   }
 }
 
+async function deletePolicyItem() {
+  const result = await this.$swal({
+    icon: "warning",
+    title: "Are you sure?",
+    html: `Are you sure you want to delete the policy item <span class="text-indigo-700">#${this.item.policy_order
+      } (${this.item.type.toTitle()})</span>? This action is irreversible.`,
+    confirmButtonText: "Delete",
+    showCancelButton: true,
+    focusConfirm: false,
+    focusCancel: true,
+  })
+  if (!result.isConfirmed) {
+    return
+  }
+  deleteEntity.call(
+    this,
+    `/policy-items/${this.item.id}`,
+    function () {
+      this.$swal({
+        icon: "success",
+        title: "Deleted token!",
+        html: `Successfully delete the policy item <span class="text-indigo-700">#${this.item.policy_order}</span>.`,
+      })
+      this.$nuxt.refresh()
+    }.bind(this)
+  )
+}
+
 module.exports = {
-  deleteEntity
+  deleteEntity,
+  deletePolicyItem
 }
