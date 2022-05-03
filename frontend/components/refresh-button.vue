e9463a60579f022ffabbe2e41328a1a0a360132d
William Oldham <wegg7250@gmail.com>
2022-05-03 07:37:33 +0100

    Frontend: spinny refresh button

--- frontend/components/refresh-button.vue
+++ frontend/components/refresh-button.vue
@@ -1,9 +1,28 @@
 <template>
-  <t-button variant="neutral" @click="$emit('click', $event)">
-    <font-awesome-icon icon="sync" />
+  <t-button variant="neutral" @click="onClick">
+    <font-awesome-icon
+      icon="sync"
+      class="transition-transform ease-in-out"
+      :class="{ 'rotate-180': spinning }"
+      @transitionend="reset" />
   </t-button>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      spinning: false,
+    }
+  },
+  methods: {
+    onClick() {
+      this.spinning = true
+      this.$emit("click", this.$event)
+    },
+    reset() {
+      this.spinning = false
+    },
+  },
+}
 </script>
