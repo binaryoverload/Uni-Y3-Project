a34a1f04950075031e08a34a7fb1625b1b27dab2
William Oldham <wegg7250@gmail.com>
2022-05-03 22:03:20 +0100

    Frontend: Input box modifications

--- frontend/components/inputs/input-box.vue
+++ frontend/components/inputs/input-box.vue
@@ -5,13 +5,14 @@
       :type="type"
       v-bind:value="value"
       v-on:input="$emit('input', $event.target.value)"
-      class="w-full bg-transparent border-0 text-slate-600 placeholder-slate-300 focus:outline-0 focus:ring-0 disabled:cursor-text"
+      class="w-full bg-transparent border-0 text-slate-600 placeholder-slate-300 focus:outline-0 focus:ring-0"
       :class="$props['input-classes']"
       :disabled="disabled"
       :id="id"
       :required="required"
       :placeholder="placeholder"
-      :maxlength="maxLength" />
+      :maxlength="maxLength"
+      v-bind="$attrs" />
   </div>
 </template>
 
