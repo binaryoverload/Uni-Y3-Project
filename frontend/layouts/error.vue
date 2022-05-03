cd4b22c1350970abb726da887c90e178abb47700
William Oldham <wegg7250@gmail.com>
2022-05-03 18:41:08 +0100

    Frontend: Start error page

---
+++ frontend/layouts/error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{ error.statusCode }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["error"],
+  layout: function ({ $auth }) {
+    if ($auth.loggedIn) {
+      return "dashboard"
+    } else {
+      return "error"
+    }
+  },
+}
+</script>
