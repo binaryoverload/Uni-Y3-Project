339fda6ef162528341909949ed43c567c5824526
William Oldham <wegg7250@gmail.com>
2022-05-03 20:47:34 +0100

    Backend: Validation for policy items shows only that items errors

--- backend/validation/policyItems.js
+++ backend/validation/policyItems.js
@@ -1,39 +1,64 @@
 const { body, param, oneOf } = require("express-validator")
 
+const fileTypeValidator = body("type", "The policy type is required")
+    .exists()
+    .trim()
+    .equals("file")
+    .withMessage("The policy ID must be file")
 const fileType = [
-    body("type", "The policy type is required")
+    fileTypeValidator,
+    body("data.file_id", "File ID is required").if(fileTypeValidator).bail().exists().trim().isUUID(4),
+    body("data.destination", "File destination is required")
+        .if(fileTypeValidator)
+        .bail()
         .exists()
         .trim()
-        .equals("file")
-        .withMessage("The policy ID must be file"),
-    body("data.file_id", "File ID is required").exists().trim().isUUID(4),
-    body("data.destination", "File destination is required").exists().trim().isLength({ min: 1 }),
-    body("data.permissions", "File permissions is required").exists().trim().isOctal(),
+        .isLength({ min: 1 }),
+    body("data.permissions", "File permissions is required").if(fileTypeValidator).bail().exists().trim().isOctal(),
 ]
 
+const packageTypeValidator = body("type", "The policy type is required")
+    .exists()
+    .trim()
+    .equals("package")
+    .withMessage("The policy ID must be package")
 const packageType = [
-    body("type", "The policy type is required")
+    packageTypeValidator,
+    body("data.action", "Package action is required")
+        .if(packageTypeValidator)
+        .bail()
         .exists()
         .trim()
-        .equals("package")
-        .withMessage("The policy ID must be package"),
-    body("data.action", "Package action is required").exists().trim().isIn(["install", "uninstall"]),
+        .isIn(["install", "uninstall"]),
     body("data.packages", "Packages are required")
+        .if(packageTypeValidator)
+        .bail()
         .exists()
         .isArray({ min: 1 })
         .withMessage("Must contain at least 1 package"),
+    body("data.packages.*", "Packages must be a slug of at most 50 characters")
+        .if(packageTypeValidator)
+        .bail()
+        .trim()
+        .isLength({ min: 1, max: 50 }),
 ]
 
+const commandTypeValidator = body("type", "The policy type is required")
+    .exists()
+    .trim()
+    .equals("command")
+    .withMessage("The policy ID must be a command")
 const commandType = [
-    body("type", "The policy type is required")
-        .exists()
-        .trim()
-        .equals("command")
-        .withMessage("The policy ID must be a command"),
-    body("data.command", "Command is required").exists().trim().isLength({ min: 1 }),
-    body("data.args", "Command arguments is required").exists().isArray(),
-    body("data.working_directory").optional({ nullable: true }).isString().isLength({ min: 1 }),
-    body("data.env").optional({ nullable: true }).isObject({ strict: true }),
+    commandTypeValidator,
+    body("data.command", "Command is required").if(commandTypeValidator).bail().exists().trim().isLength({ min: 1 }),
+    body("data.args", "Command arguments is required").if(commandTypeValidator).bail().exists().isArray(),
+    body("data.working_directory")
+        .if(commandTypeValidator)
+        .bail()
+        .optional({ nullable: true })
+        .isString()
+        .isLength({ min: 1 }),
+    body("data.env").if(commandTypeValidator).bail().optional({ nullable: true }).isObject({ strict: true }),
 ]
 
 module.exports = {
